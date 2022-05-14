5d557ca96d76c07e293ffe6abc6f75fb3625acf5
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-05-14 19:14:14 +0200

    First react component test

--- jest-react-app/src/App.test.js
+++ jest-react-app/src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Link from "./Components/Link";
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('changes the class when hovered', () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseEnter();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseLeave();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
